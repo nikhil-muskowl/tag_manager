8ce816ed63071a73145de37fd7334411fd480c0d
nikhil-muskowl <nikhil.muskowl@gmail.com>
2018-08-17 12:51:42 +0530

    naidm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import {IonTagsInputModule} from "ionic-tags-input";
 @NgModule({
   declarations: [
     MyApp,
@@ -20,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
+    IonTagsInputModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,28 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  private tags = ['Ionic', 'Angular', 'TypeScript'];
+  private hashTags = [];
   constructor(public navCtrl: NavController) {
-
+    this.bindtags();
   }
 
+  bindtags() {
+    for (let index = 0; index < this.tags.length; index++) {
+      var tag = this.tags[index];
+      if (tag.charAt(0) != '#') {
+        tag = '#' + tag;
+      }
+      this.tags.splice(index, 1, tag);
+    }
+  }
+
+  onChange() {
+    this.bindtags();
+    console.log(this.tags);
+  }
+
+  verifyTag(str: string): boolean {
+    return str !== 'ABC' && str.trim() !== '';
+  }
 }
